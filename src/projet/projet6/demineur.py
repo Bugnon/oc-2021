ca5eaeeee40f2aba261116b9b8892bdbcf9acc00
Enrico <enricop.matossi@gmail.com>
2022-05-05 15:20:45 +0200

    modifications

--- src/projet/projet6/demineur.py
+++ src/projet/projet6/demineur.py
@@ -126,25 +126,7 @@ def initialisation(o):
 #     if o == 4:
         
             
-# def joueraujeu():
-    
-# def regledujeu():
-    
-def grille(g,l,z,p):
-    for i in range(g):
-        x0 = xcor()
-        y0 = ycor()
-        up()
-        setx(x0-p)
-        sety(y0-20)
-        down()
-        for i in range(l):
-            up()
-            fd(20)
-            down()
-            for i in range(4):
-                fd(z)
-                rt(90)
+
             
 UI()
 s.listen()
