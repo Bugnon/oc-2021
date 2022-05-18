a397ba06cb6d68dd1755c436035eae38346ee009
Arcanile <arthur.delachenal@gmail.com>
2022-05-18 22:56:31 +0200

    1ère version

--- src/projet/projet8/game.py
+++ src/projet/projet8/game.py
@@ -20,7 +20,10 @@ Author: Raphael Holzer
 Date: 11 May 2022
 """
 
+from abc import get_cache_token
 from turtle import *
+from enum import Enum
+
 
 def ligne(p, q):
     """Draws a ligne from point p to point q."""
@@ -31,16 +34,21 @@ def ligne(p, q):
     up()
 
 
+class Player(Enum):
+    BLACK = 1
+    WHITE = 2
+
+
 class Rectangle:
     """Draw a filled rectangle."""
-    
+
     def __init__(self, pos, size, color='gray'):
         """Initialize the rectangle and draw it."""
         self.pos = pos
         self.size = size
         self.color = color
         self.draw()
-    
+
     def outline(self):
         """Draw just the outline of the rectangle."""
         goto(self.pos)
@@ -49,7 +57,7 @@ class Rectangle:
             forward(x)
             left(90)
         up()
-        
+
     def draw(self):
         """Draw the outline of the rectangle and fill it a color is defined."""
         if self.color:
@@ -59,18 +67,18 @@ class Rectangle:
             end_fill()
         else:
             self.outline()
-            
+
     def inside(self, p):
         """Check if the point p is inside the rectangle."""
         x, y = self.pos
         w, h = self.size
-        
+
         return 0 < p[0]-x < w and 0 < p[1]-y < h
-        
+
 
 class Text:
     """Draw a text at a given position."""
-    
+
     def __init__(self, pos, text, size=16, align='left'):
         """Initilizes the text"""
         self.pos = pos
@@ -78,25 +86,27 @@ class Text:
         self.size = size
         self.align = align
         self.draw()
-        
+
     def draw(self):
         """Draw the text."""
         goto(self.pos)
         write(self.text, font=('Arial', self.size), align=self.align)
-     
+
+
 class Button:
     def __init__(self, pos, text, size=(80, 30), color='lightgray', align='center'):
         self.rect = Rectangle(pos, size, color)
         x, y = pos
         w, h = size
         self.label = Text((x + w//2, y + h//4), text, h//2, 'center')
-        
+
     def draw(self):
         self.rect.draw()
         self.label.draw()
-    
+
     def inside(self, p):
         return self.rect.inside(p)
+
 
 class Grid:
     """Define a grid class with the size (n x m).
@@ -113,10 +123,10 @@ class Grid:
         self.ongrid = ongrid
         self.x0 = m * d // 2
         self.y0 = n * d // 2
-        
+
         self.draw()
-        print(self)
-        
+        # print(self)
+
     def draw(self):
         """Draw the grid."""
         for x in range(-self.x0, self.x0+1, self.d):
@@ -124,30 +134,31 @@ class Grid:
 
         for y in range(-self.y0, self.y0+1, self.d):
             ligne((-self.x0, y), (self.x0, y))
-    
+
     def inside(self, x, y):
         """Check if (x, y) is inside the grid."""
         x0 = self.x0
         y0 = self.y0
         if self.ongrid:
             x0 += self.d // 2
-            y0 += self.d // 2    
+            y0 += self.d // 2
         return -x0 < x < x0 and -y0 < y < y0
-            
+
     def get_cell(self, x, y):
         """Returns the coordinates of center or intersection."""
-        
+
         x += self.x0
         y += self.y0
         if self.ongrid:
             x += self.d // 2
             y += self.d // 2
-            
+
         i = int(y // self.d)
         j = int(x // self.d)
-        
-        print(i, j)
-               
+
+        #print(i, j)
+        return [i, j]
+
     def __str__(self):
         return f'Grid({self.n}, {self.m})'
 
@@ -156,50 +167,81 @@ class Game:
     """This is a general Game class.
     It contains all the game variables and attributes.
     """
-    
+
     def __init__(self):
         """Set up the game window and the turtle attributes.
         Initilize all the attributes.
         Setup the callback functions.
         """
-        
+
         setup(600, 400)
         hideturtle()
         tracer(0)
         up()
-   
+
         self.score = 0
         self.history = []
         self.grid = Grid()
-        self.title = Text((0,  170), 'Title of your Game', 24, 'center')
+        self.title = Text((0,  170), 'Othello', 24, 'center')
         self.status = Text((-280, -190), 'status line')
         self.bt_clear = Button((200, 100), 'Clear')
         self.bt_new = Button((200, 50), 'New')
-        
+        self.currentPlayer = Player.BLACK
+        self.startBoard = [[0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0]]
+        self.board = self.startBoard
+
         s = getscreen()
         s.onclick(self.click)
         s.onkey(up, 'u')
         s.onkey(down, 'd')
         s.onkey(clear, 'BackSpace')
-        s.onkey(self.draw, ' ')  
+        s.onkey(self.draw, ' ')
         s.listen()
-        
+
     def click(self, x, y):
         """Reacts to mouse clicks."""
         if self.grid.inside(x, y):
+            x = x//self.grid.d * self.grid.d + self.grid.d//2
+            y = y//self.grid.d * self.grid.d + self.grid.d//2
             goto(x, y)
-            dot()
-            write(x, y)
-        
+            if (self.registerPiece(x, y)):
+                dot(self.grid.d, self.getColor())
+                self.togglePlayer()
+
         p = x, y
         if self.bt_clear.inside(p):
             clear()
             self.bt_new.draw()
             self.bt_clear.draw()
-        
+            self.reset()
+
         if self.bt_new.inside(p):
             self.draw()
-        
+        self.grid.get_cell(x, y)
+
+    def reset(self):
+        self.board = self.tartBoard
+
+    def registerPiece(self, x, y):
+        value = -1
+        if self.grid.inside(x, y):
+            cell = self.grid.get_cell(x, y)
+            value = self.board[cell[0]-1][cell[1]-1]
+            print('Board cell {0}, {1}'.format(
+                cell, self.board[cell[0]-1][cell[1]-1]))
+        if value == 0:
+            self.board[cell[0]-1][cell[1]-1] += 1
+            return True
+        else:
+            return False
+
     def draw(self):
         """Draws all the game objects."""
         self.grid.draw()
@@ -209,4 +251,12 @@ class Game:
         self.bt_new.draw()
         down()
 
+    def togglePlayer(self):
+        self.currentPlayer = Player.BLACK if self.currentPlayer == Player.WHITE else Player.WHITE
+
+    def getColor(self):
+        return "black" if self.currentPlayer == Player.BLACK else "lightgrey"
+
+
 game = Game()
+done()
