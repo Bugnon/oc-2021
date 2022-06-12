24ad0ef9e5f93492630dd144fc44dd8d637fe59b
Louna Vizio <nalou0123@gmail.com>
2022-06-12 14:01:45 +0200

    Update snake_game.py

--- src/projet/projet5/snake_game.py
+++ src/projet/projet5/snake_game.py
@@ -8,4 +8,18 @@ delay = 0.1
 score = 0
 high_score = 0
 
-# construire l'affichage du jeu (l'écran, couleurs, etc.) 
+# définir le retard, le score et le meilleur score au point initial
+delay = 0.1
+score = 0
+high_score = 0
+
+# construire l'affichage du jeu: 
+# l'écran 
+screen = turtle.Screen()
+screen.title('Snake Game')
+screen.color('white')
+
+#la taille en largeur (width) et hauteur (height)
+screen.setup(width = 600, height = 600)
+screen.tracer(0)
+
