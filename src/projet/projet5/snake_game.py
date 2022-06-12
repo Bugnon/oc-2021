fbe5dd4fcc9e4532c5f168fc5f2c4447bc3496ab
Louna Vizio <nalou0123@gmail.com>
2022-06-12 13:55:58 +0200

    Create snake_game.py

---
+++ src/projet/projet5/snake_game.py
@@ -0,0 +1,11 @@
+# importer les différents modules pour commencer la réalisation
+from turtle import *
+import time
+import random
+
+# définir le retard, le score et le meilleur score au point initial
+delay = 0.1
+score = 0
+high_score = 0
+
+# construire l'affichage du jeu (l'écran, couleurs, etc.) 
