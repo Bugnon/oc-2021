eefd3d4c1cc057fa489505295ccd72652ef91594
Louna Vizio <nalou0123@gmail.com>
2022-06-13 20:21:15 +0200

    Update Snake_Game final.py

--- src/projet/projet5/Snake_Game final.py
+++ src/projet/projet5/Snake_Game final.py
@@ -44,4 +44,5 @@ pen.color('red')
 pen.penup()
 pen.hideturtle()
 oen.goto(250)
+# Ecrire Score et High Score et définir la police d'écriture, la taille et en gras
 pen.write("Score : 0   High Score : 0", align ="center", font = ("Arial", 28, "bold"))
